59418a2260156afdb0e4a0c2f0bf1f93e2b1b4fc
Abhishek Dixit <abhishekd@adobe.com>
2023-01-19 22:06:07 +0530

    coomit base

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import React from 'react';
+
+export default function HomePage() {
+  return (
+    <>
+      <Head>
+        <title>Home - Core WCM Component SPA Master-Page</title>
+       </Head>
+       <div>
+         Hello, this is Core WCM Component SPA Master-HomePage!
+       </div>
+    </>
+  )
+}
